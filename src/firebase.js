b67bb06e97d635e686a6a77c549dbfb045f22cd9
FrancoRosa <km115.franco@gmail.com>
2020-12-23 14:34:15 -0500

    Add firebase to project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAKiNd4YBTnaeJatCIH8rSxNS98HWwGJ5U",
+  authDomain: "tic-cusco.firebaseapp.com",
+  projectId: "tic-cusco",
+  storageBucket: "tic-cusco.appspot.com",
+  messagingSenderId: "87772616910",
+  appId: "1:87772616910:web:a14a79a63ee77e1f4d989f",
+  measurementId: "G-VV52Z4Q275"
+};
