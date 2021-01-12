43193f6725070cdd91e48c3c50f2ecd1a3a56790
FrancoRosa <km115.franco@gmail.com>
2021-01-12 15:07:31 -0500

    Render products on update

--- src/components/material/ProductPage.jsx
+++ src/components/material/ProductPage.jsx
@@ -1,61 +1,91 @@
 import { connect } from "react-redux"
 import { useHistory, useParams } from "react-router-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { useEffect, useState } from "react";
 
 const ProductPage = ({ products }) => {
   const params = useParams();
   const history = useHistory();  
-  const {
-    id,
-    title,
-    description,
-    brand, 
-    categories,
-    stock,
-    listprice,
-    price,
-    highlight,
-    list,
-    urls 
-  } = products.filter(product => product.id === params.id)[0];
+  const [product, setProduct] = useState({
+    id: '',
+    title: '',
+    description: '',
+    brand: '',
+    categories: [],
+    stock: 0,
+    price: 0,
+    urls: [],
+  })
+  
+  const updateProduct = products => {
+    if (products.length>0) {
+      const product = products.filter(product => product.id === params.id)[0];
+      const {
+        id,
+        title,
+        description,
+        brand,
+        categories,
+        stock,
+        price,
+        urls,
+      } = product;
+      setProduct({
+        id,
+        title,
+        description,
+        brand,
+        categories,
+        stock,
+        price,
+        urls,
+      });
+    }
+  }
+
+  useEffect(()=>{
+    updateProduct(products)
+  }, [products])
 
   return (
     <div>
-      <div className="navbar dashboard__nav">
-        <div className="navbar-brand">
-          <h1
-            className="title is-3 dashboard__title"
-            onClick={() => history.push('/')}
-          >
-            tic-cusco
-          </h1>
+      <Carousel 
+        showThumbs={false}
+        infiniteLoop={true}
+        autoPlay={true}
+        showStatus={false}
+        transitionTime={500}
+      >
+        <div>
+          <img className="carousel__img"
+            src="img/banner1.png" alt=""
+          />
         </div>
-        <div className="navbar-menu">
-          <div className="navbar-start new__title">
-            <h1 className="title is-6">Detalles de producto</h1>
-          </div>
-          <div className="navbar-end">
-            <button className="button is-success new__button" onClick={()=>history.push(`/edit/${id}`)}>Editar</button>  
-            <button className="button is-link new__button" onClick={()=>history.push('/dashboard')}>Regresar</button>  
-          </div>
+        <div>
+          <img className="carousel__img"
+            src="img/banner2.png" alt=""
+          />
         </div>
-      </div>
-      
+        <div>
+          <img className="carousel__img"
+            src="img/banner3.png" alt=""
+          />
+        </div>
+      </Carousel>
       <div className="container">
         <div className="card product__card">
           <table className="table product__table">
             <tbody>
-              <tr><th>Titulo:</th> <td>{title}</td></tr>
-              <tr><th>Descripcion:</th> <td>{description}</td></tr>
-              <tr><th>Marca:</th> <td>{brand}</td></tr>
-              <tr><th>Categorias ({categories.length}):</th> <td>{categories.join(', ')}</td></tr>
-              <tr><th>Destacado:</th> <td>{highlight ? 'si': 'no'}</td></tr>
-              <tr><th>Listado:</th> <td>{list ? 'si': 'no'}</td></tr>
-              <tr><th>Stock:</th> <td>{stock}</td></tr>
-              <tr><th>Precio de Lista:</th> <td>{listprice}</td></tr>
-              <tr><th>Precio:</th> <td>{price}</td></tr>
+              <tr><th>Titulo:</th> <td>{product.title}</td></tr>
+              <tr><th>Descripcion:</th> <td>{product.description}</td></tr>
+              <tr><th>Marca:</th> <td>{product.brand}</td></tr>
+              <tr><th>Categorias ({product.categories.length}):</th> <td>{product.categories.join(', ')}</td></tr>
+              <tr><th>Stock:</th> <td>{product.stock}</td></tr>
+              <tr><th>Precio:</th> <td>{product.price}</td></tr>
               <tr>
-                <th>Imagenes ({urls.length}):</th>
-                <td>{urls.map(img => <img src={img} alt="" className="product__img"/>)}</td>
+                <th>Imagenes ({product.urls.length}):</th>
+                <td>{product.urls.map(img => <img src={img} alt="" className="product__img"/>)}</td>
               </tr>
             </tbody>
           </table>
